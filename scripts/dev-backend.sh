29d70cc8d7e9068131470784bfce81bd44d87afe
Nico <aroca.twelve@proton.me>
2026-09-22 19:53:36 -0400

    updated dev script

--- scripts/dev-backend.sh
+++ scripts/dev-backend.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 cd backend
 micromamba env create -f environment.yml || micromamba env update -f environment.yml
+micromamba run -n arch-nemesis-reload-backend python -m pip install -e .
 micromamba run -n arch-nemesis-reload-backend uvicorn arch_nemesis.main:app --reload
